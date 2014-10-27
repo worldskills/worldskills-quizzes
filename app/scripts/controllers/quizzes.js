c4b43d490235c959fbd37740f04e61b169403ad2
Fabian Vogler <fabian@equivalence.ch>
2014-10-27 11:13:25 +0100

    Implement Quiz attempt

--- app/scripts/controllers/quizzes.js
+++ app/scripts/controllers/quizzes.js
@@ -13,12 +13,31 @@
         
     });
 
-    angular.module('quizzesApp').controller('QuizAttemptCtrl', function($scope, $stateParams, Quiz, QuizAttempt) {
+    angular.module('quizzesApp').controller('QuizAttemptCtrl', function($scope, $stateParams, $window, Quiz, QuizAttempt, Attempt, AttemptQuestionAnswer) {
         $scope.id = $stateParams.id;
         $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
         $scope.quiz = Quiz.get({id: $scope.id});
-        $scope.attempt = QuizAttempt.save({id: $scope.id});
-        console.log($scope.attempt)
+        $scope.questions = {};
+        $scope.attempt = QuizAttempt.save({id: $scope.id}, function () {
+            $scope.attempt.questions.forEach(function (question) {
+                if (question.answer !== null) {
+                    $scope.questions[question.id] = question.answer.id;
+                }
+            });
+        });
+        $scope.selectAnswer = function () {
+            var questionId = this.question.id;
+            var answerId = this.answer.id;
+            AttemptQuestionAnswer.update({id: $scope.attempt.id, question: questionId, answer: answerId});
+        };
+        $scope.finish = function () {
+            Attempt.finish({id: $scope.attempt.id}, function (attempt) {
+                $scope.attempt = attempt;
+            });
+        };
+        $scope.retry = function () {
+            $window.location.reload();
+        };
     });
 
 })();
--- app/scripts/services/QuizAttempt.js
+++ app/scripts/services/QuizAttempt.js
@@ -4,6 +4,30 @@
     angular.module('quizzesApp').service('QuizAttempt', function($resource, WORLDSKILLS_API_QUIZZES) {
         return $resource(WORLDSKILLS_API_QUIZZES + '/:id/attempts', {
             id: '@id'
+        }, {
+        });
+    });
+
+    angular.module('quizzesApp').service('Attempt', function($resource, WORLDSKILLS_API_QUIZZES) {
+        return $resource(WORLDSKILLS_API_QUIZZES + '/attempts/:id/:action', {
+            id: '@id'
+        }, {
+            finish: {
+                method: 'PUT',
+                params: {'action': 'finish'}
+            }
+        });
+    });
+
+    angular.module('quizzesApp').service('AttemptQuestionAnswer', function($resource, WORLDSKILLS_API_QUIZZES) {
+        return $resource(WORLDSKILLS_API_QUIZZES + '/attempts/:id/questions/:question/answers/:answer', {
+            id: '@id',
+            question: '@question',
+            answer: '@answer'
+        }, {
+            update: {
+                method: 'PUT'
+            }
         });
     });
 })();
