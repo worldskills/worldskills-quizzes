5b19e8ab7eee62ef74908dd72c8b66ae3c4fb557
Fabian Vogler <fabian@equivalence.ch>
2014-10-29 14:51:07 +0100

    Copy bootstrap fonts to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,6 +193,13 @@ module.exports = function (grunt) {
           ]
         }, {
           expand: true,
+          cwd: '<%= yeoman.app %>/bower_components/worldskills-bootstrap/fonts',
+          dest: '<%= yeoman.dist %>/fonts',
+          src: [
+            '**'
+          ]
+        }, {
+          expand: true,
           cwd: '<%= yeoman.app %>/bower_components/ckeditor',
           dest: '<%= yeoman.dist %>/bower_components/ckeditor',
           src: [
