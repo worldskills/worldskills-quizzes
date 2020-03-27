3c13c980cf010357f0b1c068f7ab62af2fabef0e
Fabian Vogler <fabian@equivalence.ch>
2020-03-27 08:37:42 +0100

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,11 +8,11 @@ export const environment = {
   worldskillsApiImages: 'http://localhost:8080/images',
   worldskillsApiEvents: 'http://localhost:8080/events',
   worldskillsApiAuth: 'http://localhost:8080/auth',
-  worldskillsClientId: '269f5d21',
-  worldskillsAuthorizeUrl: 'http://worldskills-auth.dev/oauth/authorize',
-  worldskillsAuthorizeRedirect: 'http://localhost:11300/',
-  worldskillsAuthorizeUserinfoEndpoint: 'http://worldskills-auth.dev/oauth/users/loggedIn',
-  worldskillsAuthUriPatterns: ['api.worldskills.show'],
+  worldskillsClientId: '91c518ccad27',
+  worldskillsAuthorizeUrl: 'http://worldskills-auth.localhost/oauth/authorize',
+  worldskillsAuthorizeRedirect: 'http://localhost:11301/',
+  worldskillsAuthorizeUserinfoEndpoint: 'http://worldskills-auth.localhost/oauth/users/loggedIn',
+  worldskillsAuthUriPatterns: ['localhost:8080'],
   loadChildEntityRoles: true,
   filterAuthRoles: [1300]
 };
