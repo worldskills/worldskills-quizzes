b900f48a6c966d5554cd08a89c155ac2a7d5c828
Fabian Vogler <fabian@equivalence.ch>
2020-04-23 14:02:06 +0200

    Strip all block tags and allow more styling tags

--- src/utils/striptags.ts
+++ src/utils/striptags.ts
@@ -1,7 +1,7 @@
 import * as striptags from 'striptags';
 
 export function striptagsFromText(data: string): string {
-  let text = striptags(data, ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'i', 'a', 'img', 'li', 'ol', 'ul'], '<br>');
+  let text = striptags(data, ['strong', 'b', 'i', 'em', 'a', 'img', 'sup', 'sub', 'strike', 's', 'code'], '<br>');
   text = text.replace(/(?:\s*<br>\s*){2,}/g, '<br>');
   text = text.replace(/(?:(?:^\s*<br>\s*)|(?:\s*<br>\s*)$)/g, '');
   return text;
