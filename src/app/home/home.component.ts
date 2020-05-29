57d2240aba1428da3b6fd4af7a6c1656217f0a54
Fabian Vogler <fabian@equivalence.ch>
2020-05-29 16:58:11 +0200

    Move spinner to home component
    Add <router-outlet> for non logged in user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '@worldskills/worldskills-angular-lib';
 
 @Component({
@@ -6,7 +6,7 @@ import {AuthService} from '@worldskills/worldskills-angular-lib';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit  {
 
   constructor(private authService: AuthService) {
   }
