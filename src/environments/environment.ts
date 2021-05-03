6dd06f4f899ab042bfb5259e31b998dd3e4f725b
Fabian Vogler <fabian@equivalence.ch>
2021-05-03 08:01:25 +0200

    Fix environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   environmentWarning: 'This is the local development environment. Happy coding!',
-  worldskillsApiEndpoint: 'http://localhost:8081',
-  worldskillsClientId: 'e7a23a88163b',
+  worldskillsApiEndpoint: 'http://localhost:8080',
+  worldskillsClientId: '8992d3242774',
   worldskillsAuthorizeUrl: 'http://localhost:50300/oauth/authorize',
   worldskillsAuthorizeRedirect: 'http://localhost:11300/',
   worldskillsAuthorizeUserinfoEndpoint: 'http://localhost:50300/oauth/users/loggedIn',
